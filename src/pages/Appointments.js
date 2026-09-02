cd0faa8caa2dc9f7e221db1076d51ac4fe0fa8a1
Mukesh Mahara <mukeshmahara1@gmail.com>
2026-09-03 00:56:56 +0545

    Refactor Appointments and Patients components to export directly from their respective modules

--- src/pages/Appointments.js
+++ src/pages/Appointments.js
@@ -1,11 +1,1 @@
-import React from "react";
-
-const Appointments = () => {
-  return (
-    <div>
-      <h1 className="text-2xl font-bold">Appointments</h1>
-    </div>
-  );
-};
-
-export default Appointments;
+export { Appointments as default } from "./appointments";
